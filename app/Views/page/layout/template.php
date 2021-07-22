8c19c363772e641c7219f6e45cdad0ff4b80d82e
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-07-23 00:19:28 +0700

    gtag

--- app/Views/page/layout/template.php
+++ app/Views/page/layout/template.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NHP476D');</script>
+	<!-- End Google Tag Manager -->
+
 	<!-- Etc -->
 	<meta charset="utf-8">
 	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
@@ -24,6 +32,10 @@
 </head>
 
 <body data-cookies="off" id="home" itemscope itemtype="http://schema.org/WebPage">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHP476D"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->	
 
 	<!-- Preloader -->
 	<div class="loader-mask">
